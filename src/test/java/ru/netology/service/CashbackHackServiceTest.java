94d1fb644402e550dfe854414f170019742f2cfc
Name Surname <dmitriy91pozdeev@mail.ru>
2024-01-28 14:13:18 +0300

    Added Junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1_000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @org.junit.Test
+
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+
+    }
+
+
+}
